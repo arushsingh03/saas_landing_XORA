e7dfece8abb92846c1cd33f914029a463526c2e4
Arush Singh <sarushmad@gmail.com>
2024-09-21 10:14:56 +0530

    initial commit

---
+++ src/sections/Header.jsx
@@ -0,0 +1,13 @@
+const header = () => {
+  return (
+    <html>
+      <div>
+        <a>
+          <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+        </a>
+      </div>
+    </html>
+  )
+}
+
+export default header
